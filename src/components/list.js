ff42b597fc069fc1b3f8a778980afa625fbbd143
sivaniramachandran85@gmail.com <sivaniramachandran85@gmail.com>
2024-09-14 21:59:28 +0530

    first commit

--- src/components/list.js
+++ src/components/list.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../assets/global.scss';
 
-const ListItem = ({icon, artist, name, index, data, selectedSong, setSelectedSong }) => {
-    return(
-        <div key={index}>
-            <div className={`list_item ${selectedSong.name === name ? 'selected' : ''}`} onClick={()=> setSelectedSong(data)}>
-                <div><img src={`https://cms.samespace.com/assets/${icon}`} alt="name" width={48} height={48}/></div>
-                <div>
-                    <div className="song_name">{name}</div>
-                    <div className="artist_name">{artist}</div>
-                </div>
-            </div>
+const ListItem = ({ icon, artist, name, index, data, selectedSong, setSelectedSong }) => {
+    const [duration, setDuration] = useState('');
+
+    useEffect(() => {
+        
+        const audio = new Audio(data.url);
+        audio.addEventListener('loadedmetadata', () => {
+            
+            const minutes = Math.floor(audio.duration / 60);
+            const seconds = Math.floor(audio.duration % 60);
+            setDuration(`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
+        });
+    }, [data.url]);
+
+    return (
+        <div key={index} className={`list_item ${selectedSong.name === name ? 'selected' : ''}`} onClick={() => setSelectedSong(data)}>
+            <div><img src={`https://cms.samespace.com/assets/${icon}`} alt={name} width={48} height={48} /></div>
             <div>
+                <div className="song_name">{name}</div>
+                <div className="artist_name">{artist}</div>
             </div>
+            <div className="duration">{duration}</div> 
         </div>
-    )
-}
+    );
+};
+
 export default ListItem;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://cms.samespace.com/graphql', // Replace with your GraphQL server endpoint
+  uri: 'https://cms.samespace.com/graphql', 
   cache: new InMemoryCache(),
 });
 
@@ -19,7 +19,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
